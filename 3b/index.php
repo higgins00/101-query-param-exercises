aa2a6d1c08ba4c28caf1b2098152c3922f2d8db6
Higgins <danie.higgins@gmail.com>
2016-10-24 17:51:37 -0500

    updated to step 3

--- 3b/index.php
+++ 3b/index.php
@@ -1,27 +1,91 @@
+<?php
 
-<!DOCTYPE html>
+/* ======================================================
+   PHP Calculator example using "sticky" form (Version 2)
+   ======================================================
+
+   - uses a dropdown select box to select operation
+   - does very basic validation
+   - uses a function to calculate result
+   - traps a devide by zero error
+   
+   - author : p-chatterjee, sept.2012
+   - https://gist.github.com/pchatterjee/3756368
+   
+*/
+
+// function to calculate and return result
+function calculate($num1, $num2, $op) { 
+    // calculate $prod using switch (case) statement
+    switch($op) {
+        case '+':
+            $prod = $num1 + $num2;
+            break;
+        case '-':
+            $prod = $num1 - $num2;
+            break;
+        case '*':
+            $prod = $num1 * $num2;
+            break;
+        case '/':
+            if ($num2 == 0) {$prod = "&#8734";}
+            else {$prod = $num1 / $num2;}
+    }
+
+    // return the result
+    return $prod;
+}
+
+// declare all variables
+$num1 = 0;
+$num2 = 0;
+$prod = 0;
+$op = '';
+
+// grab the form values from $_GET hash
+extract($_GET);
+
+?>
+
 <html>
-<head>
-	<title>Calculator - 3a</title>
-</head>
-<body>
-<body>
-    <p>Insert two numbers in the next form and hit submit button.</p>
+    <head>
+       <title>Calculator - 3b</title>
+    </head>
+
+    <body>
+
+      
+        <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+        <input type="text" name="num1" value="<?php print $num1; ?>"/>
+            <select name="op">
+                <option value="+" <?php if ($op=='+') echo 'selected="selected"';?>>+</option>
+                <option value="-" <?php if ($op=='-') echo 'selected="selected"';?>>-</option>
+                <option value="*" <?php if ($op=='*') echo 'selected="selected"';?>>*</option>
+                <option value="/" <?php if ($op=='/') echo 'selected="selected"';?>>/</option>
+            </select>
+        <input type="text" name="num2"  value="<?php  print $num2; ?>"/>
+        <input type="submit" name="calc" value="Calculate"/>
+        </form>
+
+    <?php
+        if(isset($calc)) {
+            // check that $num1 & $num2 are numeric
+            
+            if (is_numeric($num1) && is_numeric($num2)) {	
+                // call the caculate function and pass $num1, $num2 & $op as args.
+                $prod = calculate($num1, $num2, $op);
+				
+                // print the result 
+                echo "<p>$num1 $op $num2 = $prod</p>";
+            }
+            else {
+                // unaccepatable values
+                echo "<p>x and y values are required to be numeric ... 
+                         please re-enter values</p>";
+            }
+        }
+    ?>
     
-    <form> 
-    <input name="num1" type="text" /> 
-        <select name="oper">
-          <option value="add">&#43;</option>
-          <option value="sub">&#45;</option>
-          <option value="multi">&#42;</option>
-          <option value="divide">&#47;</option>
-        </select>
-    <input name="num2" type="text" /> 
-    <input type="submit" />
-</form>
-
-
-  </body>  
-</html>
-</body>
+  </body>
 </html>
