c0f7c10c482240eadd57322f0b670a098e714ef9
Higgins <danie.higgins@gmail.com>
2016-10-24 20:25:13 -0500

    cleaned up indents to 4

--- 4/index.php
+++ 4/index.php
@@ -4,18 +4,34 @@
 	<title>
 		4 - Color Picker!
 	</title>
+
+	<?php 
+		if ($_GET["theme"] == "red") {
+        	echo '<link rel="stylesheet" type="text/css" href="style1.css">';
+    	} elseif ($_GET["theme"] == "blue") {
+        	echo '<link rel="stylesheet" type="text/css" href="style2.css">';
+    	}
+	?>
 </head>
 <body>
 
-<?php 
-if ($_GET["theme"] = "red") {
-        echo '<link rel="stylesheet" type="text/css" href="style1.css">';
-    } if ($_GET["theme"] = "blue") {
-        echo '<link rel="stylesheet" type="text/css" href="style2.css">';
-    }
-?>
 
-<a class="theme">Switch the theme</a>
+
+	<?php
+
+		if ($_GET["theme"] == "red") {
+	?>
+
+			<a class="theme" href="./index.php?theme=blue">Switch the theme</a>
+	
+	<?php 
+		} elseif ($_GET["theme"] == "blue") {
+	?>
+
+			<a class="theme" href="./index.php?theme=red">Switch the theme</a>
+	<?php 
+		}
+	?>
 
 </body>
 </html>
